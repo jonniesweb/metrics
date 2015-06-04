b079985688de8d03c6b9835c716d14ad8dc08019
Jon Simpson <jon@jonsimpson.ca>
2015-06-03 22:03:02 -0400

    - Main class now handles getting host and app names - Removed host and app setters from MetricsConfig - Added maven-assembly-plugin to create a fat jar

--- src/main/java/ca/jonsimpson/metrics/Main.java
+++ src/main/java/ca/jonsimpson/metrics/Main.java
@@ -1,11 +1,12 @@
 package ca.jonsimpson.metrics;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricFilter;
 import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.health.HealthCheckRegistry;
 import com.codahale.metrics.jvm.ClassLoadingGaugeSet;
 import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
 import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
@@ -15,7 +16,7 @@ import com.readytalk.metrics.StatsDReporter;
 
 public class Main {
 	
-	MetricsConfig metrics = new MetricsConfig();
+	private MetricsConfig metrics;
 
 	public static void main(String[] args) {
 		new Main();
@@ -23,7 +24,8 @@ public class Main {
 
 	public Main() {
 		
-		// create a new MetricRegistry
+		// create a new MetricsConfig with the hostName and appName
+		metrics = new MetricsConfig(getHostName(), getAppName());
 		
 		// Configure reporting metrics to StatsD
 		StatsDReporter.forRegistry(metrics.getRegistry())
@@ -59,7 +61,53 @@ public class Main {
 		}
 		
 	}
+	
+	/**
+	 * Get a unique name for the machine running this application. Gets the name
+	 * from <code>hostName</code>, or if null automatically from the computer's
+	 * hostname, or if null default to <code>host</code>.
+	 * 
+	 * @return The name of the computer running this application
+	 */
+	public String getHostName() {
+		try {
+			if (System.getProperty("hostName") != null) {
+				return System.getProperty("hostName");
+			} else {
+				return InetAddress.getLocalHost().getHostName();
+				
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		return "host";
+	
+	}
+	
+	/**
+	 * Get the name of this application from the JVM parameter
+	 * <code>appName</code>. Defaults to <code>app</code> if not set.
+	 * 
+	 * @return The name of this app
+	 */
+	public String getAppName() {
+		String appName = System.getProperty("appName");
+		if (appName == null) {
+			return "app";
+		}
+		return appName;
+	}
 
+	/**
+	 * Register the following JVM metrics with the given {@link MetricRegistry}:
+	 * <li>Garbage collection
+	 * <li>Memory
+	 * <li>Threads
+	 * <li>Classes
+	 * <li>File descriptors
+	 * 
+	 * @param registry
+	 */
 	private void registerJvmMetrics(MetricRegistry registry) {
 		MetricUtils.registerAll("jvm.gc", new GarbageCollectorMetricSet(), registry);
 		MetricUtils.registerAll("jvm.memory", new MemoryUsageGaugeSet(), registry);
--- src/main/java/ca/jonsimpson/metrics/MetricsConfig.java
+++ src/main/java/ca/jonsimpson/metrics/MetricsConfig.java
@@ -1,15 +1,12 @@
 package ca.jonsimpson.metrics;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.health.HealthCheckRegistry;
 
 /**
  * All metrics action goes down here. Keeps track of the namespaces used for
- * this application. Every metric is prefixed with APPNAME.HOSTNAME. Register
- * metrics and reporters here.
+ * this application. Every metric is prefixed with appName.hostName. Register
+ * metrics and reporters to this object.
  */
 public class MetricsConfig {
 	private String hostName;
@@ -37,12 +34,6 @@ public class MetricsConfig {
 		healthChecks = new HealthCheckRegistry();
 	}
 	
-	/**
-	 * Creates a {@link MetricsConfig} 
-	 */
-	public MetricsConfig() {
-	}
-	
 	public MetricRegistry getRegistry() {
 		return registry;
 	}
@@ -59,48 +50,11 @@ public class MetricsConfig {
 		this.healthChecks = healthChecks;
 	}
 
-	/**
-	 * Get the host name of this machine from the JVM parameter
-	 * <code>hostName</code> or automatically from the computer's hostname.
-	 * 
-	 * @return The name of this computer
-	 */
 	public String getHostName() {
-	
-		if (hostName == null) {
-			try {
-				if (System.getProperty("hostName") != null) {
-					hostName = System.getProperty("hostName");
-				} else {
-					hostName = InetAddress.getLocalHost().getHostName();
-					
-				}
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			}
-		}
 		return hostName;
-	
 	}
 
-	public void setHostName(String hostName) {
-		this.hostName = hostName;
-	}
-
-	public void setAppName(String appName) {
-		this.appName = appName;
-	}
-
-	/**
-	 * Get the name of this application from the JVM parameter
-	 * <code>appName</code>
-	 * 
-	 * @return The name of this app
-	 */
 	public String getAppName() {
-		if (appName == null) {
-			appName = System.getProperty("appName");
-		}
 		return appName;
 	}
 }
